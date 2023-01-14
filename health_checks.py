9f358c9d353674d071aa50177da9e2d57e579c66
Abhinav Chadha <Abhinav22221@outlook.com>
2023-01-14 21:13:04 +0530

    updated it to make it faster used the global to write to a list instead of a file

--- health_checks.py
+++ health_checks.py
@@ -35,32 +35,24 @@ def check_no_network():
         return False
     except:
         return True
-
+okl=[]
 def run(f1,funcf):
-    ok=''
-    if f1()==True:
-        ok=funcf
-    with open('ok','a') as f:
-        if ok!='':
-            f.write('\n'+ok)
-
+    global okl
+    if f1():
+        okl.append(funcf)
 def main():
     func={check_reboot:"reboot-required!",check_root_full:'disk full!',check_cpu_constraint:'CPU load is too high!',check_no_network:'no working network!'}
     for f in func: 
         executor.submit(run,f,func[f])
     print('waiting for threads to complete')
     executor.shutdown()
-    f2= open('ok')
-    a=f2.read()[1:]
-    if a=='':
+    global okl
+    if len(okl)<1:
         print('Congrats Everything Ok!')
-        f2.close()
-        os.remove('ok')
         sys.exit(0)
-    print(a)
-    f2.close()
-    os.remove('ok')
-    sys.exit(1)
+    for x in okl:
+        print(x)
+    sys.exit(len(okl))
        
 if __name__=="__main__":
     main()
